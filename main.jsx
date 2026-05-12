cd6f64e49d9464a64be7210c5a0ebab5155d55c2
aaravsha0711-png <aaravsha0711@gmail.com>
2026-05-11 23:41:06 -0500

    Add shared score route to frontend entry point

--- main.jsx
+++ main.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./score-reader.jsx";
+import SharedScorePage from "./SharedScorePage.jsx";
 import { ThemeProvider } from "./theme.jsx";
 import "./theme.css";
+
+function Root() {
+  const match = window.location.pathname.match(/^\/shared\/([^/]+)$/);
+  if (match) {
+    return <SharedScorePage token={match[1]} />;
+  }
+  return <App />;
+}
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
+      <Root />
     </ThemeProvider>
   </React.StrictMode>
 );
