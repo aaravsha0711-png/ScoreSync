1a1c601d0f6a815655d273ee51da665f36e15948
aaravsha0711-png <aaravsha0711@gmail.com>
2026-05-11 23:31:00 -0500

    Wrap application with ThemeProvider

--- main.jsx
+++ main.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./score-reader.jsx";
+import { ThemeProvider } from "./theme.jsx";
+import "./theme.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
